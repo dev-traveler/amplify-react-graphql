6e76dafdfc0a4587031d92d89a3b742b68454270
Jaehyun <recreate@spass.ai>
2022-11-03 11:12:37 +0900

    Setup a React application tied to an AWS Amplify backend and configured Amplify's UI components

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,9 @@
+export type AmplifyDependentResourcesAttributes = {
+    "api": {
+        "firsttest": {
+            "GraphQLAPIKeyOutput": "string",
+            "GraphQLAPIIdOutput": "string",
+            "GraphQLAPIEndpointOutput": "string"
+        }
+    }
+}
